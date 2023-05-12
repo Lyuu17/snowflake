ce2126325ad6d0d544256a83764e7a18c0c749e4
Lyuu17 <lyuu17@yandex.com>
2023-05-12 11:27:24 +0200

    Add greetd service

--- hosts/lucx/default.nix
+++ hosts/lucx/default.nix
@@ -6,4 +6,17 @@
   programs = {
     hyprland.enable = true;
   };
+
+  services = {
+    greetd = {
+      enable = true;
+      settings = rec {
+        initial_session = {
+          command = "Hyprland";
+          user = "lucx";
+        };
+        default_session = initial_session;
+      };
+    };
+  };
 }
